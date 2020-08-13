0d9db55b9c0983abf6c5cc3f3d27f568fc64e1de
Mohit krishna <mohitkrishna.s.s12@gmail.com>
2020-08-13 15:25:52 +0530

    db update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs=require("ejs");
 const md5=require("md5");
 
 
-mongoose.connect("mongodb+srv://admin-login:process.env.PASS@cluster0.fdebt.mongodb.net/formdb",{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://admin-login:Drobotic@solutions@cluster0.fdebt.mongodb.net/formdb",{useNewUrlParser:true,useUnifiedTopology:true});
 
 
 const app=express();
